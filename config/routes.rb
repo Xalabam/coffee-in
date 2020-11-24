158eafa7967703e3001413d117684fa89340b507
Alex Tchaikovsky <xalabam94@icloud.com>
2020-11-24 16:50:33 +0800

    created controller for coupons

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :coupons, only: [:index, :show, :create, :destroy]
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :coupons, only: [:index, :show, :create, :destroy]
+      get "/users/id/coupons", to: "coupons#my_coupons", as: 'my_coupons'
+    end
+  end
 
-  get "/users/id/coupons", to: "coupons#my_coupons", as: 'my_coupons'
-
-  resources :claims, only: [:index, :create, :destroy]
+  namespace :api, defaults: {format: :json} do
+    namespace :v1 do
+      resources :claims, only: [:index, :create, :destroy]
+    end
+  end
 end

119c02c5a91455fb3bbd2e0775ef7592559f7f1a
Alex Tchaikovsky <xalabam94@icloud.com>
2020-11-24 11:04:24 +0800

    created routes for coupons

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :coupons, only: [:index, :show, :create, :destroy]
+
+  get "/users/id/coupons", to: "coupons#my_coupons", as: 'my_coupons'
+
 end

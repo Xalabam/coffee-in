e0343858fe3090cde61bfe2ede73d93b455d318f
Ariel Xu <arielxu@live.com>
2020-11-25 13:11:27 +0800

    updated the database in the user model by adding open id column

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,7 +1,7 @@
-class Api::V1::LoginController < ApplicationController
+class LoginController < Api::V1::BaseController
+  skip_before_action :verify_authenticity_token, only: [:login]
 
   URL = "https://api.weixin.qq.com/sns/jscode2session".freeze
-
 
   def wechat_user
     wechat_params = {
@@ -18,7 +18,7 @@ class Api::V1::LoginController < ApplicationController
   def login
     @user = User.find_or_create_by(open_id: wechat_user.fetch("openid"))
     render json: {
-      userId: @user.id
+      user: @user
     }
   end
 end
